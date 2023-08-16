c704ac70c395e98efb46de8d51c71184c1687967
A Ray <a@machinaut.com>
2023-08-15 19:37:46 -0700

    make a memory

--- src/tt_um_machinaut_systolic.v
+++ src/tt_um_machinaut_systolic.v
@@ -1,6 +1,6 @@
 `default_nettype none
 
-module tt_um_machinaut_systolic #( parameter MAX_COUNT = 24'd10_000_000 ) (
+module tt_um_machinaut_systolic #( parameter MEMSIZE = 8 ) (
     input  wire [7:0] ui_in,    // Dedicated inputs - connected to the input switches
     output wire [7:0] uo_out,   // Dedicated outputs - connected to the 7 segment display
     input  wire [7:0] uio_in,   // IOs: Bidirectional Input path
@@ -12,49 +12,34 @@ module tt_um_machinaut_systolic #( parameter MAX_COUNT = 24'd10_000_000 ) (
 );
 
     wire reset = ! rst_n;
-    wire [6:0] led_out;
-    assign uo_out[6:0] = led_out;
-    assign uo_out[7] = 1'b0;
+    wire [7:0] data_in;
+    wire [7:0] data_out;
+    wire [6:0] addr;
+    wire save;
+    assign addr = ui_in[6:0];
+    assign save = ui_in[7];
+    assign data_in = uio_in;
+    assign uo_out = data_out;
 
-    // use bidirectionals as outputs
-    assign uio_oe = 8'b11111111;
+    // All bidirectional IOs are inputs by default
+    assign uio_oe = 8'b00000000;
 
-    // put bottom 8 bits of second counter out on the bidirectional gpio
-    assign uio_out = second_counter[7:0];
+    // Register array
+    reg [7:0] mem [0:MEMSIZE];
 
-    // external clock is 10MHz, so need 24 bit counter
-    reg [23:0] second_counter;
-    reg [3:0] digit;
-
-    // if external inputs are set then use that as compare count
-    // otherwise use the hard coded MAX_COUNT
-    wire [23:0] compare = ui_in == 0 ? MAX_COUNT: {6'b0, ui_in[7:0], 10'b0};
-
-    always @(posedge clk) begin
-        // if reset, set counter to 0
+    always @(posedge clk or posedge reset) begin
+        // if reset
         if (reset) begin
-            second_counter <= 0;
-            digit <= 0;
+            mem <= 0;
         end else begin
-            // if up to 16e6
-            if (second_counter == compare) begin
-                // reset
-                second_counter <= 0;
-
-                // increment digit
-                digit <= digit + 1'b1;
-
-                // only count from 0 to 9
-                if (digit == 9)
-                    digit <= 0;
-
-            end else
-                // increment counter
-                second_counter <= second_counter + 1'b1;
+            // if save is high
+            if (save) begin
+                // save data_in to mem[addr]
+                mem[addr] <= data_in;
+            end
+            // output mem[addr] to data_out
+            data_out <= mem[addr];
         end
-    end
-
-    // instantiate segment display
-    seg7 seg7(.counter(digit), .segments(led_out));
+    end end
 
 endmodule
