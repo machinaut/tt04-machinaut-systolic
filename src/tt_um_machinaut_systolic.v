2979bf1e34a39afb25b44c9c7b8832392a0d326b
A Ray <a@machinaut.com>
2023-08-15 19:50:29 -0700

    fix syntax

--- src/tt_um_machinaut_systolic.v
+++ src/tt_um_machinaut_systolic.v
@@ -1,6 +1,6 @@
 `default_nettype none
 
-module tt_um_machinaut_systolic #( parameter MEMSIZE = 8 ) (
+module tt_um_machinaut_systolic (
     input  wire [7:0] ui_in,    // Dedicated inputs - connected to the input switches
     output wire [7:0] uo_out,   // Dedicated outputs - connected to the 7 segment display
     input  wire [7:0] uio_in,   // IOs: Bidirectional Input path
@@ -13,33 +13,31 @@ module tt_um_machinaut_systolic #( parameter MEMSIZE = 8 ) (
 
     wire reset = ! rst_n;
     wire [7:0] data_in;
-    wire [7:0] data_out;
     wire [6:0] addr;
     wire save;
     assign addr = ui_in[6:0];
     assign save = ui_in[7];
     assign data_in = uio_in;
-    assign uo_out = data_out;
 
     // All bidirectional IOs are inputs by default
     assign uio_oe = 8'b00000000;
 
     // Register array
-    reg [7:0] mem [0:MEMSIZE];
+    reg [7:0] mem ;
+    assign uo_out = mem;
 
     always @(posedge clk or posedge reset) begin
         // if reset
         if (reset) begin
+            // Set all memory to zero
             mem <= 0;
         end else begin
             // if save is high
             if (save) begin
-                // save data_in to mem[addr]
-                mem[addr] <= data_in;
+                // save data_in to mem
+                mem <= data_in;
             end
-            // output mem[addr] to data_out
-            data_out <= mem[addr];
         end
-    end end
+    end
 
 endmodule
