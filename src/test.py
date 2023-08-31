5e6bae376416a37cb13c87a094ea3c532598c4c0
A Ray <a@machinaut.com>
2023-08-30 23:15:28 -0700

    reading in and out edge vectors

--- src/test.py
+++ src/test.py
@@ -5,10 +5,8 @@ from cocotb.clock import Clock
 from cocotb.triggers import RisingEdge, FallingEdge, Timer, ClockCycles
 
 
-segments = [ 63, 6, 91, 79, 102, 109, 124, 7, 127, 103 ]
-
 @cocotb.test()
-async def test_7seg(dut):
+async def test_basic(dut):
     dut._log.info("start")
     clock = Clock(dut.clk, 10, units="us")
     cocotb.start_soon(clock.start())
@@ -16,35 +14,25 @@ async def test_7seg(dut):
     # reset
     dut._log.info("reset")
     dut.rst_n.value = 0
-    # set the compare value
-    dut.ui_in.value = 1
-    await ClockCycles(dut.clk, 10)
+    await ClockCycles(dut.clk, 1)
     dut.rst_n.value = 1
+    # Read in values
+    dut.uio_in.value = 0
+    # Go through two blocks
+    for i in range(16):
+        dut.ui_in.value = 64 + i
+        await ClockCycles(dut.clk, 1)
+        assert dut.uo_out.value == 0, f"i={i}, dut.uo_out.value={dut.uo_out.value}"
+    # Go through second block
+    for i in range(16):
+        dut.ui_in.value = 96 + i
+        await ClockCycles(dut.clk, 1)
+        assert dut.uo_out.value == 64 + i, f"i={i}, dut.uo_out.value={dut.uo_out.value}"
 
-    # the compare value is shifted 10 bits inside the design to allow slower counting
-    max_count = dut.ui_in.value << 10
-    dut._log.info(f"check all segments with MAX_COUNT set to {max_count}")
-    # check all segments and roll over
-    for i in range(15):
-        dut._log.info("check segment {}".format(i))
-        await ClockCycles(dut.clk, max_count)
-        assert int(dut.segments.value) == segments[i % 10]
-
-        # all bidirectionals are set to output
-        assert dut.uio_oe == 0xFF
-
-    # reset
+    # Reset again
+    dut._log.info("reset")
     dut.rst_n.value = 0
-    # set a different compare value
-    dut.ui_in.value = 3
-    await ClockCycles(dut.clk, 10)
+    await ClockCycles(dut.clk, 2)
     dut.rst_n.value = 1
-
-    max_count = dut.ui_in.value << 10
-    dut._log.info(f"check all segments with MAX_COUNT set to {max_count}")
-    # check all segments and roll over
-    for i in range(15):
-        dut._log.info("check segment {}".format(i))
-        await ClockCycles(dut.clk, max_count)
-        assert int(dut.segments.value) == segments[i % 10]
+    assert dut.uo_out.value == 0
 
--- src/tt_um_machinaut_systolic.v
+++ src/tt_um_machinaut_systolic.v
@@ -11,11 +11,11 @@ module tt_um_machinaut_systolic (
     input  wire       rst_n     // reset_n - low to reset
 );
     // state - final dimension is block, read in 0, read out 1
-    reg [15:0][3:0] ain;  // A Input (BFloat16 4-vector)
-    reg [15:0][3:0] bin;  // B Input (BFloat16 4-vector)
-    reg [15:0][3:0] a;  // A (BFloat16 4-vector)
-    reg [15:0][3:0] b;  // B (BFloat16 4-vector)
-    reg [31:0][15:0] c;  // FP32 16-vector C
+    reg [0:3][15:0] ain;  // A Input (BFloat16 4-vector)
+    reg [0:3][15:0] bin;  // B Input (BFloat16 4-vector)
+    reg [0:3][15:0] a;  // A (BFloat16 4-vector)
+    reg [0:3][15:0] b;  // B (BFloat16 4-vector)
+    reg [0:15][31:0] c;  // FP32 16-vector C
     reg [5:0] state;
     // alias labels for state
     wire [1:0] cout_byte;
@@ -52,49 +52,50 @@ module tt_um_machinaut_systolic (
             b <= '0;
             c <= '0;
             state <= '0;
+            uout <= '0;
         end else begin
             if (!run_n) begin  // Run Systolic
                 // Systolic pipeline
                 state[3:0] <= state[3:0] + 1;
+                if (state[3:0] == 15) begin // Rollover pipeline, move blocks
+                    a <= ain;
+                    b <= {bin[0], bin[1], bin[2], bin[3][15:8], ui_in};
+                end
                 if (!ab_sel) begin  // A vector
                     case (ab_addr)
-                        0: begin case (ab_byte) 0: ain[0][15:8] <= ui_in; 0: ain[0][7:0] <= ui_in; endcase end
-                        1: begin case (ab_byte) 0: ain[1][15:8] <= ui_in; 0: ain[1][7:0] <= ui_in; endcase end
-                        2: begin case (ab_byte) 0: ain[2][15:8] <= ui_in; 0: ain[2][7:0] <= ui_in; endcase end
-                        3: begin case (ab_byte) 0: ain[3][15:8] <= ui_in; 0: ain[3][7:0] <= ui_in; endcase end
+                        0: case (ab_byte) 0: begin ain[0][15:8] <= ui_in; uout <= a[0][7:0]; end 1: begin ain[0][7:0] <= ui_in; uout <= a[1][15:8]; end endcase
+                        1: case (ab_byte) 0: begin ain[1][15:8] <= ui_in; uout <= a[1][7:0]; end 1: begin ain[1][7:0] <= ui_in; uout <= a[2][15:8]; end endcase
+                        2: case (ab_byte) 0: begin ain[2][15:8] <= ui_in; uout <= a[2][7:0]; end 1: begin ain[2][7:0] <= ui_in; uout <= a[3][15:8]; end endcase
+                        3: case (ab_byte) 0: begin ain[3][15:8] <= ui_in; uout <= a[3][7:0]; end 1: begin ain[3][7:0] <= ui_in; uout <= b[0][15:8]; end endcase
                     endcase
                 end else begin  // B vector
                     case (ab_addr)
-                        0: begin case (ab_byte) 0: bin[0][15:8] <= ui_in; 0: bin[0][7:0] <= ui_in; endcase end
-                        1: begin case (ab_byte) 0: bin[1][15:8] <= ui_in; 0: bin[1][7:0] <= ui_in; endcase end
-                        2: begin case (ab_byte) 0: bin[2][15:8] <= ui_in; 0: bin[2][7:0] <= ui_in; endcase end
-                        3: begin case (ab_byte) 0: bin[3][15:8] <= ui_in; 0: bin[3][7:0] <= ui_in; endcase end
+                        0: case (ab_byte) 0: begin bin[0][15:8] <= ui_in; uout <= b[0][7:0]; end 1: begin bin[0][7:0] <= ui_in; uout <= b[1][15:8]; end endcase
+                        1: case (ab_byte) 0: begin bin[1][15:8] <= ui_in; uout <= b[1][7:0]; end 1: begin bin[1][7:0] <= ui_in; uout <= b[2][15:8]; end endcase
+                        2: case (ab_byte) 0: begin bin[2][15:8] <= ui_in; uout <= b[2][7:0]; end 1: begin bin[2][7:0] <= ui_in; uout <= b[3][15:8]; end endcase
+                        3: case (ab_byte) 0: begin bin[3][15:8] <= ui_in; uout <= b[3][7:0]; end 1: begin bin[3][7:0] <= ui_in; uout <= ain[0][15:8]; end endcase
                     endcase
-                end
-                if (!state[3:0]) begin // Rollover pipeline, move blocks
-                    a <= ain;
-                    b <= bin;
                 end
             end else begin
                 // Read out C vector
                 state[5:0] <= state[5:0] + 1;
                 case (cout_state)
-                    0: begin case (cout_byte) 0: uout <= c[0][31:24]; 1: uout <= c[0][23:16]; 2: uout <= c[0][15:8]; 3: uout <= c[0][7:0]; endcase end
-                    1: begin case (cout_byte) 0: uout <= c[1][31:24]; 1: uout <= c[1][23:16]; 2: uout <= c[1][15:8]; 3: uout <= c[1][7:0]; endcase end
-                    2: begin case (cout_byte) 0: uout <= c[2][31:24]; 1: uout <= c[2][23:16]; 2: uout <= c[2][15:8]; 3: uout <= c[2][7:0]; endcase end
-                    3: begin case (cout_byte) 0: uout <= c[3][31:24]; 1: uout <= c[3][23:16]; 2: uout <= c[3][15:8]; 3: uout <= c[3][7:0]; endcase end
-                    4: begin case (cout_byte) 0: uout <= c[4][31:24]; 1: uout <= c[4][23:16]; 2: uout <= c[4][15:8]; 3: uout <= c[4][7:0]; endcase end
-                    5: begin case (cout_byte) 0: uout <= c[5][31:24]; 1: uout <= c[5][23:16]; 2: uout <= c[5][15:8]; 3: uout <= c[5][7:0]; endcase end
-                    6: begin case (cout_byte) 0: uout <= c[6][31:24]; 1: uout <= c[6][23:16]; 2: uout <= c[6][15:8]; 3: uout <= c[6][7:0]; endcase end
-                    7: begin case (cout_byte) 0: uout <= c[7][31:24]; 1: uout <= c[7][23:16]; 2: uout <= c[7][15:8]; 3: uout <= c[7][7:0]; endcase end
-                    8: begin case (cout_byte) 0: uout <= c[8][31:24]; 1: uout <= c[8][23:16]; 2: uout <= c[8][15:8]; 3: uout <= c[8][7:0]; endcase end
-                    9: begin case (cout_byte) 0: uout <= c[9][31:24]; 1: uout <= c[9][23:16]; 2: uout <= c[9][15:8]; 3: uout <= c[9][7:0]; endcase end
-                    10: begin case (cout_byte) 0: uout <= c[10][31:24]; 1: uout <= c[10][23:16]; 2: uout <= c[10][15:8]; 3: uout <= c[10][7:0]; endcase end
-                    11: begin case (cout_byte) 0: uout <= c[11][31:24]; 1: uout <= c[11][23:16]; 2: uout <= c[11][15:8]; 3: uout <= c[11][7:0]; endcase end
-                    12: begin case (cout_byte) 0: uout <= c[12][31:24]; 1: uout <= c[12][23:16]; 2: uout <= c[12][15:8]; 3: uout <= c[12][7:0]; endcase end
-                    13: begin case (cout_byte) 0: uout <= c[13][31:24]; 1: uout <= c[13][23:16]; 2: uout <= c[13][15:8]; 3: uout <= c[13][7:0]; endcase end
-                    14: begin case (cout_byte) 0: uout <= c[14][31:24]; 1: uout <= c[14][23:16]; 2: uout <= c[14][15:8]; 3: uout <= c[14][7:0]; endcase end
-                    15: begin case (cout_byte) 0: uout <= c[15][31:24]; 1: uout <= c[15][23:16]; 2: uout <= c[15][15:8]; 3: uout <= c[15][7:0]; endcase end
+                    0: case (cout_byte) 0: uout <= c[0][31:24]; 1: uout <= c[0][23:16]; 2: uout <= c[0][15:8]; 3: uout <= c[0][7:0]; endcase
+                    1: case (cout_byte) 0: uout <= c[1][31:24]; 1: uout <= c[1][23:16]; 2: uout <= c[1][15:8]; 3: uout <= c[1][7:0]; endcase
+                    2: case (cout_byte) 0: uout <= c[2][31:24]; 1: uout <= c[2][23:16]; 2: uout <= c[2][15:8]; 3: uout <= c[2][7:0]; endcase
+                    3: case (cout_byte) 0: uout <= c[3][31:24]; 1: uout <= c[3][23:16]; 2: uout <= c[3][15:8]; 3: uout <= c[3][7:0]; endcase
+                    4: case (cout_byte) 0: uout <= c[4][31:24]; 1: uout <= c[4][23:16]; 2: uout <= c[4][15:8]; 3: uout <= c[4][7:0]; endcase
+                    5: case (cout_byte) 0: uout <= c[5][31:24]; 1: uout <= c[5][23:16]; 2: uout <= c[5][15:8]; 3: uout <= c[5][7:0]; endcase
+                    6: case (cout_byte) 0: uout <= c[6][31:24]; 1: uout <= c[6][23:16]; 2: uout <= c[6][15:8]; 3: uout <= c[6][7:0]; endcase
+                    7: case (cout_byte) 0: uout <= c[7][31:24]; 1: uout <= c[7][23:16]; 2: uout <= c[7][15:8]; 3: uout <= c[7][7:0]; endcase
+                    8: case (cout_byte) 0: uout <= c[8][31:24]; 1: uout <= c[8][23:16]; 2: uout <= c[8][15:8]; 3: uout <= c[8][7:0]; endcase
+                    9: case (cout_byte) 0: uout <= c[9][31:24]; 1: uout <= c[9][23:16]; 2: uout <= c[9][15:8]; 3: uout <= c[9][7:0]; endcase
+                    10: case (cout_byte) 0: uout <= c[10][31:24]; 1: uout <= c[10][23:16]; 2: uout <= c[10][15:8]; 3: uout <= c[10][7:0]; endcase
+                    11: case (cout_byte) 0: uout <= c[11][31:24]; 1: uout <= c[11][23:16]; 2: uout <= c[11][15:8]; 3: uout <= c[11][7:0]; endcase
+                    12: case (cout_byte) 0: uout <= c[12][31:24]; 1: uout <= c[12][23:16]; 2: uout <= c[12][15:8]; 3: uout <= c[12][7:0]; endcase
+                    13: case (cout_byte) 0: uout <= c[13][31:24]; 1: uout <= c[13][23:16]; 2: uout <= c[13][15:8]; 3: uout <= c[13][7:0]; endcase
+                    14: case (cout_byte) 0: uout <= c[14][31:24]; 1: uout <= c[14][23:16]; 2: uout <= c[14][15:8]; 3: uout <= c[14][7:0]; endcase
+                    15: case (cout_byte) 0: uout <= c[15][31:24]; 1: uout <= c[15][23:16]; 2: uout <= c[15][15:8]; 3: uout <= c[15][7:0]; endcase
                 endcase
             end
         end
